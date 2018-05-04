096b3eb01640ece0317fe119c5438acf7af84547
zhiyilv <zhiyilv@outlook.com>
2018-05-05 01:05:54 +0800

    update springer

--- HIspider/endpoint.py
+++ HIspider/endpoint.py
@@ -1,9 +1,9 @@
 from scrapy.cmdline import execute
 
 
-# execute(['scrapy', 'crawl', 'springer'])
+execute(['scrapy', 'crawl', 'springer'])
 # execute(['scrapy', 'crawl', 'elsevier'])
-execute(['scrapy', 'crawl', 'taylor'])
+# execute(['scrapy', 'crawl', 'taylor'])
 
 
 
